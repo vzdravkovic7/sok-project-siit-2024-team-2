840ee5503189c67e30f69c8f290f91dd26438bd8
David Stakic <davidstakic2003@gmail.com>
2025-08-24 10:35:59 +0200

    [REFACTOR] Minor Changes

--- simple_visualizer/graph/simple/plugin.py
+++ simple_visualizer/graph/simple/plugin.py
@@ -40,7 +40,7 @@ class SimpleVisualizer(VisualizerPlugin):
         """
         Converts the graph into a structured format for visualization.
         """
-        nodes = [{"id": node.node_id, "name": str(node.value), "x": float(node.x), "y": float(node.y)} for node in graph.nodes]
+        nodes = [{"id": node.node_id, "name": str(node.values), "x": float(node.x), "y": float(node.y)} for node in graph.nodes]
         edges = [
             {"from": edge.from_node.node_id, "to": edge.to_node.node_id, "weight": edge.weight}
             for edge in graph.edges if edge.to_node is not None
